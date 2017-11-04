61667868f0aec31a858673774bffa16de1a6f0c9
Jon Egeland <jonegeland@gmail.com>
2017-11-04 16:52:21 -0400

    Add initial formula definition.

---
+++ Formula/myst-lang.rb
@@ -0,0 +1,31 @@
+class MystLang < Formula
+  desc "The Myst programming language"
+  homepage "https://github.com/myst-lang/myst"
+  url "https://github.com/myst-lang/myst/archive/v0.1.0.tar.gz"
+  sha256 "8bd6904696f1f4d21edce474c99077e8478943e15188be1932e588923916a6a3"
+
+  depends_on "crystal-lang" => :build
+
+  def install
+    # Install the executable binary
+    system "crystal", "build", "./src/myst.cr"
+    bin.install "myst"
+    # Install the interpreter source for clients to interact with.
+    prefix.install "src"
+    # Install the standard library
+    prefix.install "stdlib"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test myst-lang`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "#{bin}/myst"
+  end
+end
---
+++ README.md
@@ -0,0 +1,17 @@
+# Myst-lang Tap
+
+A custom tap for installing versions of the [Myst programming language](https://github.com/myst-lang/myst).
+
+## How do I install these formulae?
+`brew install myst-lang/tap/<formula>`
+
+Or `brew tap myst-lang/tap` and then `brew install <formula>`.
+
+Or install via URL (which will not receive updates):
+
+```
+brew install https://raw.githubusercontent.com/myst-lang/homebrew-tap/master/Formula/<formula>.rb
+```
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
